b9a164b84a164ad2ffb97ad07b443f8c6749e8fe
minh tri <trinvmgcd18384@fpt.edu.vn>
2020-12-26 09:58:56 +0700

    use Hooks in exe2

--- quadratic_equation/src/App.js
+++ quadratic_equation/src/App.js
@@ -1,72 +1,111 @@
 import './App.css';
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputA:0,
-      inputB:0,
-      inputC:0,
-      result:""
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       inputA:0,
+//       inputB:0,
+//       inputC:0,
+//       result:""
 
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+//     };
+//     this.handleChange = this.handleChange.bind(this);
+//     this.handleSubmit = this.handleSubmit.bind(this);
 
-  }
+//   }
   
-  handleChange(event) {
-    const value = event.target.value;
-    const name = event.target.name;
-    this.setState({
-      [name]: value
-    });
-  }
+//   handleChange(event) {
+//     const value = event.target.value;
+//     const name = event.target.name;
+//     this.setState({
+//       [name]: value
+//     });
+//   }
 
-  handleSubmit(event) {
+//   handleSubmit(event) {
+//     event.preventDefault();
+//     if(this.state.inputA === 0) {
+//       this.setState({
+//         result: `Phương trình có một nghiệm đơn x = ${-this.state.inputC/this.state.inputB}`
+//       })
+//     } 
+    // else if(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC < 0){
+    //   this.setState({
+    //     result: "Phương trình vô nghiệm"    
+    //   });
+    // }
+    // else if(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC === 0){
+    //   this.setState({
+    //     result: `Phương trình có một nghiệm kép X = ${-this.state.inputB/(2*this.state.inputA)}`
+    //   })
+    // }
+    // else{
+    //   this.setState({
+    //     result: `Phương trình có hai nghiệm phân biệt 
+    //     x1 = ${(- this.state.inputB + Math.sqrt(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC))/(2*this.state.inputA)}
+    //     và x2 = ${(- this.state.inputB - Math.sqrt(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC))/(2*this.state.inputA)}`
+    // });
+//   }
+// }
+
+//   render() {
+//     return (
+      // <div className="App">
+      //   <h1>Giải phương trình bậc 2 </h1>
+      //   <form>
+      //     <input type="number" name="inputA" value={this.state.inputA} onChange={this.handleChange}></input>
+      //     <h2>X<sup>2</sup>+</h2>
+      //     <input type="number" name="inputB" value={this.state.inputB} onChange={this.handleChange}></input>
+      //     <h2>X+</h2>
+      //     <input type="number" name="inputC" value={this.state.inputC} onChange={this.handleChange}></input>
+      //     <h2>= 0</h2>
+      //     <button onClick={this.handleSubmit}>Tính</button>
+      //   </form>
+      //   <h2>{this.state.result}</h2>
+      // </div>
+//     );
+//   }
+// }
+
+const App = () => {
+  const [a,setA] = useState(null);
+  const [b,setB] = useState(null);
+  const [c,setC] = useState(null);
+  const [result,setResult] = useState(null);
+
+  const calculate = (event) => {
     event.preventDefault();
-    if(this.state.inputA === 0) {
-      this.setState({
-        result: `Phương trình có một nghiệm đơn x = ${-this.state.inputC/this.state.inputB}`
-      })
-    } 
-    else if(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC < 0){
-      this.setState({
-        result: "Phương trình vô nghiệm"    
-      });
+    if(Math.pow(b,2) - 4*a*c < 0){
+      setResult("Phương trình vô nghiệm");    
     }
-    else if(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC === 0){
-      this.setState({
-        result: `Phương trình có một nghiệm kép X = ${-this.state.inputB/(2*this.state.inputA)}`
-      })
+    else if(Math.pow(b,2) - 4*a*c === 0){
+      setResult(`Phương trình có một nghiệm kép X = ${-b/(2*a)}`);
     }
     else{
-      this.setState({
-        result: `Phương trình có hai nghiệm phân biệt 
-        x1 = ${(- this.state.inputB + Math.sqrt(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC))/(2*this.state.inputA)}
-        và x2 = ${(- this.state.inputB - Math.sqrt(Math.pow(this.state.inputB,2) - 4*this.state.inputA*this.state.inputC))/(2*this.state.inputA)}`
-    });
+        setResult(`Phương trình có hai nghiệm phân biệt 
+        x1 = ${(- b + Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a)}
+        và x2 = ${(- b - Math.sqrt(Math.pow(b,2) - 4*a*c))/(2*a)}`);
+    }
   }
-}
 
-  render() {
-    return (
-      <div className="App">
+  return(
+    <div className="App">
         <h1>Giải phương trình bậc 2 </h1>
         <form>
-          <input type="number" name="inputA" value={this.state.inputA} onChange={this.handleChange}></input>
+          <input type="number" placeholder="a" className="math-input" onChange={(e) => setA(e.target.valueAsNumber)}></input>
           <h2>X<sup>2</sup>+</h2>
-          <input type="number" name="inputB" value={this.state.inputB} onChange={this.handleChange}></input>
+          <input type="number" placeholder="b" className="math-input" onChange={(e) => setB(e.target.valueAsNumber)}></input>
           <h2>X+</h2>
-          <input type="number" name="inputC" value={this.state.inputC} onChange={this.handleChange}></input>
+          <input type="number" placeholder="c" className="math-input" onChange={(e) => setC(e.target.valueAsNumber)}></input>
           <h2>= 0</h2>
-          <button onClick={this.handleSubmit}>Tính</button>
+          <button onClick={calculate}>Tính</button>
         </form>
-        <h2>{this.state.result}</h2>
+        {result != null && (<h2>{result}</h2>)}
       </div>
-    );
-  }
+  )
+
 }
 
 export default App;
